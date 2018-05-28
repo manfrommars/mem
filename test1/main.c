eafe84e5d030e7cecb947d821f3d2533e7af5062
Elliot Simon <elliot.a.simon@gmail.com>
2018-05-28 15:49:17 -0700

    Linked list implementation with default (test1) and ASAN-enabled (test1-asan) builds

---
+++ test1/main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+struct node {
+    int i;
+    struct node* next;
+};
+
+void append_new_node_to_list(struct node** list) {
+    struct node* n = calloc(1, sizeof(struct node));
+    if (*list) {
+        while ((*list)->next) {
+            (*list) = (*list)->next;
+        }
+        (*list)->next = n;
+    } else {
+        *list = n;
+    }
+}
+
+void destroy_linked_list(struct node* list) {
+    while (list) {
+        struct node* temp = list;
+        list = list->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct node* list = NULL;
+    append_new_node_to_list(&list);
+    append_new_node_to_list(&list);
+
+    destroy_linked_list(list);
+    return 0;
+}
