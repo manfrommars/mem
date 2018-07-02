e6e5904a1e4e23dcc6dbfd3cf35d4de25f96f372
Elliot Simon <elliot.a.simon@gmail.com>
2018-07-02 13:06:33 -0400

    Updated to include function call

--- test2/main.c
+++ test2/main.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 
-int main() {
-    int  a = 42;
-    int* b = &a;
+int func1(int* a) {
+    if (*a > 100) {
+        *a = 1;
+        return 100;
+    }
+    else {
+        *a = 2;
+        return 101;
+    }
+}
 
+int main() {
+    int  a = 200;
+    int* b = &a;
+    int  c = func1(b);
+
+    printf(" c: %d\n", c);
     printf(" a: %d\n", a);
+    printf("&a: %p\n", &a);
     printf(" b: %p\n", b);
     printf("*b: %d\n", *b);
 
-    // Reset a
+    // Oops
     b = 0;
 
+    c = func1(b);
+
+    printf(" c: %d\n", c);
     printf(" a: %d\n", a);
+    printf("&a: %p\n", &a);
     printf(" b: %p\n", b);
     printf("*b: %d\n", *b);
 
